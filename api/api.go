0cf3d615b360fb7c1aeb31ec1c0dac8394478f1a
Shamit Mishra <shamitmishra22@gmail.com>
2025-05-10 12:37:46 +0530

    Initialize go module and add basic project structure
    This commit initializes the go module and adds the basic project structure, including the `api/` and `cmd/` directories. This provides a foundation for building the application.

---
+++ api/api.go
@@ -0,0 +1,20 @@
+package api
+
+import (
+	"encoding/json"
+	"net/http"
+)
+
+type CoinBalanceParams struct {
+	Username string
+}
+
+type CoinBalanceResponse struct {
+	Code int
+	Balance int
+}
+
+type Error struct {
+	Code int
+	Message string
+}
---
+++ go.mod
@@ -0,0 +1,3 @@
+module github.com/iamshamit/goapi
+
+go 1.24.3
